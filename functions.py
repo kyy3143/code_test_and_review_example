86cd5b47a28df880b8443724ffe0d9f912a9e950
unknown <youngsang6716@gmail.com>
2025-05-20 18:49:41 +0900

    feat: add divide(a, b) function to functions.py

--- functions.py
+++ functions.py
@@ -5,3 +5,10 @@ def add(a, b):
 def minus(a, b):
     """Subtract b from a."""
     return a - b
+
+def divide(a,b):
+    """divide function"""
+    if b == 0 :
+        return("나눌수 없음")
+    else:
+        return a/b
